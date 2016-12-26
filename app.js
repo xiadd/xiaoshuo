0363068674903aa63eb2a133cefbe476355f7e5c
xiadd <1278609551@qq.com>
2016-12-26 17:30:30 +0800

    ''

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('trust proxy', 1)
 app.use(session({
   store: new RedisStore({ host: '127.0.0.1', port: '6379' }),
   secret: 'xiadd',
-  resave: false,
+  resave: true,
   saveUninitialized: false,
   name: 'ywid'
 }))
